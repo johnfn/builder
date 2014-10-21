0dbda7604e8b7f43d734f654c23a3ad11f470c7b
johnfn <johnfn@gmail.com>
2014-10-20 21:23:22 -0700

    Random map generation.

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,98 @@ class G {
   static game:Phaser.Game;
 }
 
+interface Point {
+  x: number
+  y: number
+}
+
+function make2dArray(size:number, val:any) {
+  var result = [];
+
+  for (var i = 0; i < size; i++) {
+    result[i] = [];
+
+    for (var j = 0; j < size; j++) {
+      result[i][j] = val;
+    }
+  }
+
+  return result;
+}
+
 class GameMap {
   tiles:Phaser.Sprite[][];
+  grid:number[][];
 
   public constructor() {
+    this.generateTerrain(5);
+    this.normalizeGrid()
+    this.quantizeGrid(4);
+
     for (var i = 0; i < 20; i++) {
       for (var j = 0; j < 20; j++) {
         var x:number = i * 32;
         var y:number = j * 32;
 
-        G.game.add.sprite(x, y, "tiles", Math.floor(Math.random() * 4));
+        G.game.add.sprite(x, y, "tiles", this.grid[i][j]);
       }
     }
   }
+
+  quantizeGrid(options:number) {
+    for (var i = 0; i < 20; i++) {
+      for (var j = 0; j < 20; j++) {
+        this.grid[i][j] = Math.floor(this.grid[i][j] * options);
+      }
+    }
+  }
+
+  normalizeGrid() {
+    var min:number = Number.POSITIVE_INFINITY;
+    var max:number = Number.NEGATIVE_INFINITY;
+
+    for (var i = 0; i < 20; i++) {
+      for (var j = 0; j < 20; j++) {
+        var val:number = this.grid[i][j];
+
+        if (val > max) max = val;
+        if (val < min) min = val;
+      }
+    }
+
+    for (var i = 0; i < 20; i++) {
+      for (var j = 0; j < 20; j++) {
+        this.grid[i][j] = (this.grid[i][j] - min) / max;
+      }
+    }
+  }
+
+  generateTerrain(smoothness:number) {
+    var deltas:Point[] = [{x: 0, y: 1}, {x: 0, y: -1},{x: 1, y: 0},{x: -1, y: 0}]
+    var grid:number[][] = make2dArray(20, 1);
+
+    for (var iteration = 0; iteration < smoothness; iteration++) {
+      for (var i = 0; i < 20; i++) {
+        for (var j = 0; j < 20; j++) {
+          var neighborScores:number = 0;
+          var neighbors:number = 0;
+
+          for (var k = 0; k < deltas.length; k++) {
+            var new_i:number = i + deltas[k].x;
+            var new_j:number = j + deltas[k].y;
+            if (new_i < 0 || new_i >= grid.length || new_j < 0 || new_j >= grid[0].length) continue;
+            neighborScores += grid[new_i][new_j];
+            neighbors++;
+          }
+
+          grid[i][j] = (neighborScores / neighbors) + (6 * Math.random() - 3) / (iteration + 1)
+        }
+      }
+    }
+
+    this.grid = grid;
+  }
+
 }
 
 class MainState extends Phaser.State {
