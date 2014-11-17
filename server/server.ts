81a7ce395c535e006270f3775afc685e1c336cb8
johnfn <johnfn@gmail.com>
2014-11-16 16:23:10 -0800

    Finally get a server sorta-working.

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,5 @@
-/// <reference path="../d/express.d.ts" />
-/// <reference path="../d/node.d.ts" />
+declare var exports:any;
 
-import express = require('express');
-import http = require("http")
-var app = express();
-
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+exports.f = function() {
+  return "blah";
+}
